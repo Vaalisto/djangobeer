f902bca116b4f42912820ea30c77a5d04687f29b
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-07-31 14:33:36 +0300

    List of breweries now uses render

--- beer/views.py
+++ beer/views.py
@@ -1,14 +1,11 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Brewery
 
 def index(request):
-	all_breweries = Brewery.objects.all()
-	template = loader.get_template('beer/index.html')
-	context = {
-		'all_breweries': all_breweries,
-	}
-	return HttpResponse(template.render(context, request))
+	all_breweries = Brewery.objects.all()	
+	context = {'all_breweries': all_breweries}
+	return render(request, 'beer/index.html', context)
 
 def detail(request, brewery_id):
 	return HttpResponse("<h2>Details for Brewery id: " + str(brewery_id) + "</h2>")
