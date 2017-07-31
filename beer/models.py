84f1f194f4289ff4e0236a70a97ad4b154d8ebef
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-07-31 13:33:42 +0300

    Made models brewery and beer

--- beer/models.py
+++ beer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Brewery(models.Model):
+	name = models.CharField(max_length=250)
+	year = models.PositiveIntegerField()
+
+	def __str__(self):
+		return self.name + ', ' + str(self.year)
+
+class Beer(models.Model):
+	name = models.CharField(max_length=250)
+	brewery = models.ForeignKey(Brewery, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.name
--- beerdjango/settings.py
+++ beerdjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'beer.apps.BeerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
