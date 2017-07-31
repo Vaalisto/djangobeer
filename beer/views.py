75c011df4bd6eee040e6b4d39515dbb789e933a0
Ville Vaalisto <ville.vaalisto@gmail.com>
2017-07-31 14:27:15 +0300

    Breweries are now listed using simple template

--- beer/views.py
+++ beer/views.py
@@ -1,13 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
 from .models import Brewery
 
 def index(request):
 	all_breweries = Brewery.objects.all()
-	html = ''
-	for brewery in all_breweries:
-		url = '/beer/' + str(brewery.id) + '/'
-		html += '<a href="' + url + '">' + brewery.name + '</a><br>'
-	return HttpResponse(html)
+	template = loader.get_template('beer/index.html')
+	context = {
+		'all_breweries': all_breweries,
+	}
+	return HttpResponse(template.render(context, request))
 
 def detail(request, brewery_id):
 	return HttpResponse("<h2>Details for Brewery id: " + str(brewery_id) + "</h2>")
